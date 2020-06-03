c5f3ef84ef874dd5897fb8ea9e314829c6f3a25c
mxahan <smxahsan@gmail.com>
2020-06-03 04:13:42 -0400

    new

--- dl_pytorch_offical.py
+++ dl_pytorch_offical.py
@@ -136,3 +136,118 @@ print(x.requires_grad)
 y = x.detach()
 print(y.requires_grad)
 print(x.eq(y).all())
+
+
+#%% Neural network 
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        
+        self.fc1 = nn.Linear(16*6*6, 120)
+        self.fc2 = nn.Linear(120, 84)
+        
+        self.fc3 = nn.Linear(84, 10)
+        
+    def forward(self, x):
+        
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        
+        x = x.view(-1, self.num_flat_features(x))
+        
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        
+        x = self.fc3(x)
+        
+        return x
+    def num_flat_features(self, x):
+        
+        size = x.size()[1:]
+        
+        num_features = 1 
+        for s in size:
+            num_features= num_features*s
+        return num_features
+            
+    
+net = Net()
+
+print(net)
+
+#%% Parametes
+
+params =  list(net.parameters()) # like trainables in tf
+
+# list like params[0].data
+print(len(params))
+print(params[0].size())
+
+
+#%%
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+net.zero_grad()
+out.backward(torch.randn(1, 10))  # random gradient
+
+#%% Loss function
+
+output = net(input)
+target = torch.randn(10)  # a dummy target, for example
+target = target.view(1, -1)  # make it the same shape as output
+criterion = nn.MSELoss()
+
+loss = criterion(output, target)
+print(loss)
+#%%
+print(loss.grad_fn)  # MSELoss
+print(loss.grad_fn.next_functions[0][0])  # Linear
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
+
+
+net.zero_grad()     # zeroes the gradient buffers of all parameters
+
+print('conv1.bias.grad before backward')
+print(net.conv1.bias.grad)  # naming interesting like 
+# net.conv1.weight.grad
+
+loss.backward()
+
+print('conv1.bias.grad after backward')
+print(net.conv1.bias.grad)
+
+
+learning_rate = 0.01
+for f in net.parameters():
+    f.data.sub_(f.grad.data * learning_rate)
+    
+#or the followings 
+
+#%%
+
+import torch.optim as optim
+
+# create your optimizer
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+# in your training loop:
+optimizer.zero_grad()   # zero the gradient buffers
+output = net(input)
+loss = criterion(output, target)
+loss.backward()
+optimizer.step()    # Does the update
+    
+
+#%% training a classifier parts
