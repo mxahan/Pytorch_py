7a5931afb56364f443c419cdccb29ea70c227286
mxahan <smxahsan@gmail.com>
2020-09-06 15:05:25 -0400

    new

--- dl_torch_offi_exam.py
+++ dl_torch_offi_exam.py
@@ -2,6 +2,256 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep  4 02:55:48 2020
-
-@author: zahid
+Source Code:
+https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 """
+#%% Numpy instead of Tensor from official 
+
+''' we can use numpy instead of pytorch tensor, but in that case will 
+have to compute all the gradient by ourself alone with the update procedure
+'''
+
+import numpy as np
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data
+x = np.random.randn(N, D_in)
+y = np.random.randn(N, D_out)
+
+# Randomly initialize weights
+w1 = np.random.randn(D_in, H)  # not tensor
+w2 = np.random.randn(H, D_out)
+
+lr =  1e-6
+
+for t in range(50):
+    # Forward pass: compute predicted y
+    h = x.dot(w1)
+    h_relu = np.maximum(h, 0)
+    y_pred = h_relu.dot(w2)
+
+    # Compute and print loss
+    loss = np.square(y_pred - y).sum()
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.copy()
+    grad_h[h < 0] = 0
+    grad_w1 = x.T.dot(grad_h)
+
+    # Update weights
+    w1 -= lr * grad_w1
+    w2 -= lr * grad_w2
+    
+#%% Tensor implementation 
+"""but still not using the automatic gradient [similar to tensorflow gradient calculation]
+Please check the tensorflow my codes summarized.
+
+Else nothing to see here
+
+"""
+
+
+import torch
+
+
+dtype = torch.float
+device = torch.device("cuda:0")
+# device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item()
+    if t % 100 == 99:
+        print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # Update weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+    
+
+#%% Now the interesing part [Auto Grad]
+
+import torch
+
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random Tensors to hold input and outputs.
+# Setting requires_grad=False indicates that we do not need to compute gradients
+# with respect to these Tensors during the backward pass.
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+# Create random Tensors for weights.
+# Setting requires_grad=True indicates that we want to compute gradients with
+# respect to these Tensors during the backward pass.
+w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+
+lr = 1e-6
+
+for t in range(200):
+    y_pred =  x.mm(w1).clamp(min=0).mm(w2)
+    
+    loss =  (y_pred-y).pow(2).sum()
+    
+    if t%100==99:
+        print(t, loss.item()) # .item() to get the value
+        
+        
+    # gradient 
+    loss.backward()
+    # w1.grad stores the gradient value 
+    
+    with torch.no_grad():
+        w1 -= learning_rate * w1.grad
+        w2 -= learning_rate * w2.grad
+
+        # Manually zero the gradients after updating weights
+        w1.grad.zero_()
+        w2.grad.zero_()
+    
+#%% Lets go big with nn module
+
+
+import torch
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random Tensors to hold inputs and outputs
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
+
+# Use the nn package to define our model as a sequence of layers. nn.Sequential
+# is a Module which contains other Modules, and applies them in sequence to
+# produce its output. Each Linear Module computes output from input using a
+# linear function, and holds internal Tensors for its weight and bias.
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
+# similar to keras.models.sequential
+
+# The nn package also contains definitions of popular loss functions; in this
+# case we will use Mean Squared Error (MSE) as our loss function.
+loss_fn = torch.nn.MSELoss(reduction='sum')
+
+learning_rate = 1e-4
+
+optimizer =  torch.optim.Adam(model.parameters(), lr = learning_rate)
+for t in range(500):
+    # Forward pass: compute predicted y by passing x to the model. Module objects
+    # override the __call__ operator so you can call them like functions. When
+    # doing so you pass a Tensor of input data to the Module and it produces
+    # a Tensor of output data.
+    y_pred = model(x)
+    # Compute and print loss. We pass Tensors containing the predicted and true
+    # values of y, and the loss function returns a Tensor containing the
+    # loss.
+    loss = loss_fn(y_pred, y)
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    # Zero the gradients before running the backward pass.
+    model.zero_grad()
+
+    # Backward pass: compute gradient of the loss with respect to all the learnable
+    # parameters of the model. Internally, the parameters of each Module are stored
+    # in Tensors with requires_grad=True, so this call will compute gradients for
+    # all learnable parameters in the model.
+    loss.backward()
+
+    # Update the weights using gradient descent. Each parameter is a Tensor, so
+    # we can access its gradients like we did before.
+    if 1==1:
+        with torch.no_grad():
+            for param in model.parameters():
+                param -= learning_rate * param.grad
+    else:
+        optimizer.step()
+    # or use optimizer
+    
+            
+#%% Pytorch Custom nn modules
+
+''' here we enherit the nn class to build new model classes
+The principles are same as before
+instead of sequential we will define new class
+'''
+
+class twonet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(twonet, self).__init__()
+        self.lin1 = torch.nn.Linear(D_in, H)
+        self.lin2 = torch.nn.Linear(H, D_out)
+        
+    def forward(self, x):
+        h_relu = self.lin1(x).clamp(min= 0)
+        y_pred = self.lin2(h_relu)
+        return y_pred
+        
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
+
+# Construct our model by instantiating the class defined above
+model = twonet(D_in, H, D_out)
+
+# loss function defined as class instance
+
+criterion =  torch.nn.MSELoss(reduction='sum')
+optimizer =  torch.optim.SGD(model.parameters(), lr = 1e-4)
+
+for t in range(500):
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x)
+
+    # Compute and print loss
+    loss = criterion(y_pred, y)
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
